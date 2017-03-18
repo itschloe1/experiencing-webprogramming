6846154003f75fb023c8761d9e30a623b55f9f35
itschloe1 <hwpark0502@gmail.com>
2017-03-18 19:21:33 +0900

    Add files via upload

---
+++ project-wk5-final.py
@@ -0,0 +1,17 @@
+def multiplication_dan():
+    try:
+        dan = int(input("몇 단을 출력 하시겠습니까?"))
+        if (dan<2 or dan>9):
+            print("2에서 9사이의 숫자만 입력해주세요")
+            return multiplication_dan()
+        else :
+            return dan
+    except ValueError:
+        print("숫자가 아닙니다.")
+
+def multiplication_rslt(dan):
+    for num in range(1, 10):
+        print("{} * {} = {}".format(dan, num, dan * num))
+
+dan = multiplication_dan()
+multiplication_rslt(dan)
